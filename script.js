a199d6b5010d4ccb9c1dc9e58e2f3ca7c7676c4f
Nick Boisclair <nboisclair64@gmail.com>
2020-05-29 17:21:18 -0400

    change

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myFunction(){
+    var x = document.getElementById("recipe1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+
+
+}
+
