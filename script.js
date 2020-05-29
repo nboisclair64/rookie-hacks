1f60b5e99ba5f259dbd4c0b14a348925d7aef08e
Nick Boisclair <nboisclair64@gmail.com>
2020-05-29 17:38:08 -0400

    button change

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 function myFunction(){
     var x = document.getElementById("recipe1");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+   
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
 
 
