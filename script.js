4d2d7f05a3c9ecf0e1ff8f760703c78c9c8f3cbf
Lauren2544 <66128679+Lauren2544@users.noreply.github.com>
2020-06-12 15:07:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function myFunction4(){
 }
 
 
-function myFunction() {
+function myFunction5() {
   var x = document.createElement("VAR");
   var t = document.createTextNode("Mille-Feuille – French Yield: One Pan Ingredients: Graham wafers, Box of Instant Vanilla Pudding, 35% whipping cream, 1 ½ cups icing sugar, 1 tsp. cocoa Directions: 1.  Layer 8" x 8" pan with:  2. Graham wafers in whole  3.  A box of Instant Vanilla Pudding  4.  A small carton 35% cream whipped  5.  Another layer of graham wafers on top in same direction as bottom layer  6. Topping 1 ½ cups icing sugar mixed with teensy weensy bit of hot water to make a thin but pourable icing (Save a couple of tablespoons of icing and mix 1 tsp. cocoa with it)  7. Ice top of wafers with white icing mixture.  8. Drizzle thin chocolate icing mixture over top and make square pattern.  9.  Refrigerate overnight or at least four hours. Can be doubled and made in a 9" x 13". Enjoy!");
   x.appendChild(t);
